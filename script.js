abb2f260b972cdf257447826bd43defed6051fb4
Diogo-P-Carvalho <dpcarvalho86@gmail.com>
2021-05-07 20:18:15 +0100

    adding function to select the streaming media

---
+++ script.js
@@ -0,0 +1,15 @@
+const videoElement = document.querySelector('#video');
+const button = document.querySelector('#button');
+
+// Propmt to select media stream, pass to video element, then play
+async function selectMediaStream() {
+    try {
+        
+    } catch (error) {
+        // catch error here
+        console.log(error);
+    }
+}
+
+// On Load
+selectMediaStream();
